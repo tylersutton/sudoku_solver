600fccd52f838993d6a63ac8fefd6842f30155a6
tylersutton <42106198+tylersutton@users.noreply.github.com>
2018-08-10 23:41:03 -0500

    Add files via upload

--- sudoku.cpp
+++ sudoku.cpp
@@ -19,25 +19,21 @@ Sudoku Sudoku::createSudoku() {
 }
 
 bool Sudoku::solveSudoku(Sudoku *sudoku) {
-	if (sudoku->isSolved(sudoku) == true) {
+	if (sudoku->isSolved(sudoku) == true)
 		return true;
-	}
 	int n = findEmpty(sudoku);
 	for (int x = 1; x <= 9; x++) {
-		if (sudoku->cell[n].in == false) {
+		if (sudoku->cell[n].in == false)
 			sudoku->cell[n].val = x;
-		}
 		if (sudoku->isValid(sudoku)) {
 			//system("clear");
 			//sudoku->displaySudoku(sudoku);			
-			if (sudoku->solveSudoku(sudoku) == true) {
+			if (sudoku->solveSudoku(sudoku) == true)
 				return true;
-			}
 		}
 	}
-	if (sudoku->cell[n].in == false) {
+	if (sudoku->cell[n].in == false)
 		sudoku->cell[n].val = 0;
-	}
 	return false;
 }
 
@@ -51,17 +47,14 @@ bool Sudoku::isValid(Sudoku *sudoku) {
 			int y = sudoku->cell[(9*j)+i].val;
 			A[x] += 1;
 			B[y] += 1;
-			if (x < 0 || x > 9) {
+			if (x < 0 || x > 9)
 				return 0;
-			}
 		}
 		for (int k = 0; k < 9; k++) {
-			if (A[k+1] > 1) {
+			if (A[k+1] > 1) 
 				return 0;
-			}
-			if (B[k+1] > 1) {
+			if (B[k+1] > 1)
 				return 0;
-			}
 		}
 	}
 	//checks boxes
@@ -75,9 +68,8 @@ bool Sudoku::isValid(Sudoku *sudoku) {
 				}
 			}
 			for (int b = 0; b < 9; b++) {
-				if (A[b+1] > 1) {
+				if (A[b+1] > 1)
 					return 0;
-				}
 			}
 		}
 	}
@@ -86,13 +78,11 @@ bool Sudoku::isValid(Sudoku *sudoku) {
 
 bool Sudoku::isSolved(Sudoku *sudoku) {
 	for (int i = 0; i < 81; i++) {
-		if (sudoku->cell[i].val == 0) {
+		if (sudoku->cell[i].val == 0)
 			return false;
-		}
 	}
-	if (sudoku->isValid(sudoku) == false) {
+	if (sudoku->isValid(sudoku) == false)
 		return false;
-	}
 	else {
 		sudoku->displaySudoku(sudoku);
 		return true;
@@ -102,32 +92,25 @@ bool Sudoku::isSolved(Sudoku *sudoku) {
 void Sudoku::displaySudoku(Sudoku *sudoku) {
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
-			if (sudoku->cell[(9*i)+j].val == 0) {
+			if (sudoku->cell[(9*i)+j].val == 0) 
 				cout << "  ";
-			}
-			else {
+			else 
 				cout << sudoku->cell[(9*i)+j].val << " ";
-			}
-			if (((j+1) % 3 == 0) && (j != 0) && (j != 8)) {
+			if (((j+1) % 3 == 0) && (j != 0) && (j != 8))
 				cout << "| ";
-			}
-			else if (j == 8) {
+			else if (j == 8)
 				cout << endl;
-			}
 		}
-		if (((i + 1) % 3 == 0) && (i != 0) && (i != 8)) {
+		if (((i + 1) % 3 == 0) && (i != 0) && (i != 8)) 
 			cout << "---------------------" << endl;
-		}
-		if (i == 8) {
+		if (i == 8) 
 			cout << endl << endl << endl;
-		}
 	}
 }
 
 int Sudoku::findEmpty(Sudoku *sudoku) {
 	for (int n = 0; n < 81; n++) {
-		if (sudoku->cell[n].val == 0) {
+		if (sudoku->cell[n].val == 0)
 			return n;
-		}
 	}
 }
